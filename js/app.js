7f66c756ceb9bccf3a4e4e37a9146d124d07d115
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-03-14 20:27:39 +0200

    added buttons + javascript edit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// const sections =document.querySelectorAll("section")
+
+// for (i = 0; i < sections.length; i++) {
+//     sections[i].addEventListener
+// }
+
+const main = document.querySelector(".main-hero");
+
+function scrollToTop() {
+    main.scrollIntoView(true);
+}
